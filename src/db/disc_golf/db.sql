45284649b282a5885a124eb21b307b2ccda9b9dc
Jose Alonzo <pepemxl@gmail.com>
2025-05-14 19:50:08 -0600

    Fix tab

--- src/db/disc_golf/db.sql
+++ src/db/disc_golf/db.sql
@@ -1,53 +1,53 @@
 create table players (
-  id bigint primary key generated always as identity,
-  name text not null,
-  country text,
-  rating int,
-  pdga_number int unique
+    id bigint primary key generated always as identity,
+    name text not null,
+    country text,
+    rating int,
+    pdga_number int unique
 );
 
 create table player_discs (
-  id bigint primary key generated always as identity,
-  disc_id bigint references discs (id),
-  country text,
-  rating int,
-  pdga_number int unique
+    id bigint primary key generated always as identity,
+    disc_id bigint references discs (id),
+    country text,
+    rating int,
+    pdga_number int unique
 );
 
 create table courses (
-  id bigint primary key generated always as identity,
-  name text not null,
-  location text,
-  number_of_holes int
+    id bigint primary key generated always as identity,
+    name text not null,
+    location text,
+    number_of_holes int
 );
 
 create table tournaments (
-  id bigint primary key generated always as identity,
-  name text not null,
-  course_id bigint references courses (id),
-  date date
+    id bigint primary key generated always as identity,
+    name text not null,
+    course_id bigint references courses (id),
+    date date
 );
 
 create table player_statistics (
-  id bigint primary key generated always as identity,
-  player_id bigint references players (id),
-  tournament_id bigint references tournaments (id),
-  score int,
-  "position" int
+    id bigint primary key generated always as identity,
+    player_id bigint references players (id),
+    tournament_id bigint references tournaments (id),
+    score int,
+    position int
 );
 
 create table disc_brands (
-  id bigint primary key generated always as identity,
-  name text not null unique
+    id bigint primary key generated always as identity,
+    name text not null unique
 );
 
 create table discs (
-  id bigint primary key generated always as identity,
-  name text not null,
-  brand_id bigint references disc_brands (id),
-  type text,
-  speed int,
-  glide int,
-  turn int,
-  fade int
+    id bigint primary key generated always as identity,
+    name text not null,
+    brand_id bigint references disc_brands (id),
+    type text,
+    speed int,
+    glide int,
+    turn int,
+    fade int
 );
